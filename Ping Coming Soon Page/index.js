97ed6de0c5f584505b063cd865bd068472772152
Taranties <99305356+Taranties@users.noreply.github.com>
2023-01-23 01:53:26 +1300

    Minor changes

--- Ping Coming Soon Page/index.js
+++ Ping Coming Soon Page/index.js
@@ -2,12 +2,15 @@ const form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
   const emailInput = document.querySelector("input[type='email']");
   const email = emailInput.value;
   const emailRegex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   if (!emailRegex.test(email)) {
     let errorMessage = document.querySelector("#error-message");
+
     if (!errorMessage) {
       const p = document.createElement("p");
       p.textContent = "Please provide a valid email address.";
@@ -18,10 +21,31 @@ form.addEventListener("submit", (event) => {
     }
   } else {
     let errorMessage = document.querySelector("#error-message");
+
     if (errorMessage) {
       form.removeChild(errorMessage);
       emailInput.classList.remove("email-error");
     }
+
     form.submit();
   }
 });
+
+const icons = document.querySelector(".icons");
+const iconDivs = icons.querySelectorAll("div");
+
+for (let i = 0; i < iconDivs.length; i++) {
+  const currentDiv = iconDivs[i];
+
+  currentDiv.addEventListener("mouseover", function () {
+    const iconSVG = currentDiv.querySelector("svg");
+    iconSVG.classList.add("svg-hover-colour");
+    currentDiv.classList.add("icons-hover");
+  });
+
+  currentDiv.addEventListener("mouseout", function () {
+    const iconSVG = currentDiv.querySelector("svg");
+    iconSVG.classList.remove("svg-hover-colour");
+    currentDiv.classList.remove("icons-hover");
+  });
+}
