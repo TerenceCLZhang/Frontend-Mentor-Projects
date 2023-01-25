0b9c03047f4f6f33f1882f7659adb40f55b645c1
Taranties <99305356+Taranties@users.noreply.github.com>
2023-01-25 15:48:55 +1300

    Update script.js

--- Advice Generator App/script.js
+++ Advice Generator App/script.js
@@ -2,18 +2,18 @@ const adviceButton = document.getElementById("get-advice");
 const adviceText = document.getElementById("advice");
 const adviceNumber = document.getElementById("number");
 
-adviceButton.addEventListener("click", async () => {
+const loadAdvice = async () => {
   try {
-    const response = await fetch("https://api.adviceslip.com/advice");
-    const data = await response.json();
-    adviceText.innerText = data.slip.advice;
-    adviceNumber.innerText = data.slip.id;
+    const {
+      slip: { advice, id },
+    } = await (await fetch("https://api.adviceslip.com/advice")).json();
+    adviceText.innerText = advice;
+    adviceNumber.innerText = id;
   } catch (err) {
     adviceText.innerText = "Error getting advice";
     adviceNumber.innerText = "Error getting advice number";
   }
-});
-
-window.onload = function () {
-  adviceButton.click();
 };
+
+loadAdvice();
+adviceButton.addEventListener("click", loadAdvice);
