6e75becadab18c53037d1a42dcab2ff1749d0354
Taranties <99305356+Taranties@users.noreply.github.com>
2023-02-23 22:29:54 +1300

    Initial Commit

---
+++ Results Summary Component/scripts/LoadResults.js
@@ -0,0 +1,21 @@
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const dataArray = data.map((item) => ({
+      category: item.category,
+      score: item.score,
+      icon: item.icon,
+    }));
+
+    const statisticElements = document.querySelectorAll(".statistic");
+    statisticElements.forEach((statistic, index) => {
+      const category = dataArray[index].category;
+      const score = dataArray[index].score;
+      const icon = dataArray[index].icon;
+
+      statistic.classList.add(category.toLowerCase());
+      statistic.querySelector("img").setAttribute("src", icon);
+      statistic.querySelector("#number").textContent = score;
+    });
+  })
+  .catch((error) => console.error(error));
